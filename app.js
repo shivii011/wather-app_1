438e6a0508a6e82644b7a0946231fc9de3e5a985
shivii011 <shiv011kush011@gamil.com>
2022-08-15 20:56:05 +0530

    change in api

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ window.addEventListener("load" , ()=>{
         long = position.coords.longitude;
         lat = position.coords.latitude;
         console.log(long , lat)
-        const api = `https://api.openweathermap.org/data/2.5/weather?lat=${77.8960201 }&lon=${21.901160}&appid={6b7647225e796d5074670d07ebdfebea
+        const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid={6b7647225e796d5074670d07ebdfebea
     }`
+    //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
         fetch(api)
         .then(response=>{
